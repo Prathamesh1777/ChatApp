cdbc8aff55babdea31acd8118de2ff853d5f9b65
Prathamesh1777 <aarv2000@gmail.com>
2025-04-19 15:35:56 +0530

    register api created

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express=require('express');
 const app=express();
 const port=3030;
 const condb=require('./config/db');
+const User=require('./models/User');
 
-
+app.use(express.json());
 condb();
 
 
@@ -13,13 +14,36 @@ res.json({
     data :['Prathamesh','aarv']   
 });  
 
-
-
-
 });
+
+
+app.post('/register',(req,res)=>{
+
+    try{   
+   const newUser= new User({
+   username : req.body.username,
+   password : req.body.username
+   
+   }) ;   
+   
+   newUser.save();
+   
+   res.status(201).json({
+       message : "registration successfull"
+   })
+   
+    } catch(error){
+        res.status(500).json({
+            error:"internal server error"
+        })
+
+    }
+   
+   });
+
+
 
 app.listen(port,()=>{
 
 console.log(`Server listening to ${port}`);
-
 });
