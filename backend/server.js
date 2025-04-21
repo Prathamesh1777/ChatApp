c9b3eafab6bb1fe7747728dc97f45424f5427138
Prathamesh1777 <aarv2000@gmail.com>
2025-04-21 22:15:33 +0530

    Password encrypted

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const port=3030;
 const condb=require('./config/db');
 const User=require('./models/User');
 
+const bcrypt=require('bcrypt');
+
 app.use(express.json());
 condb();
 
@@ -19,25 +21,42 @@ res.json({
 
 app.post('/register',(req,res)=>{
 
-    try{   
-   const newUser= new User({
-   username : req.body.username,
-   password : req.body.username
-   
-   }) ;   
-   
-   newUser.save();
-   
-   res.status(201).json({
-       message : "registration successfull"
-   })
-   
-    } catch(error){
-        res.status(500).json({
-            error:"internal server error"
-        })
+    const saltRounds = 10; // Recommended value
+    const plainPassword = req.body.password;
+    
+    
+    bcrypt.hash(plainPassword, saltRounds, function(err, hash) {
+      if (err) {
+        console.log('password encryption failed');
+      } else {
+        // Store the hash in your database
+        
+        console.log('Hashed password:', hash);
 
-    }
+
+        try{   
+            const newUser= new User({
+            username : req.body.username,
+            password : hash
+            
+            }) ;   
+            
+            newUser.save();
+            
+            res.status(201).json({
+                message : "registration successfull"
+            })
+            
+             } catch(error){
+                 res.status(500).json({
+                     error:"internal server error"
+                 })
+         
+             }
+      }
+    });
+
+   
    
    });
 
